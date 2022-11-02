ee63f6c368ce3269cfacb6775e46447adee839ee
Maaaksimus <plowecz@mail.ru>
2022-11-02 18:17:22 +0300

    add formu;as

--- main.c
+++ main.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <math.h>
 
 #define left 0
 #define right 1
@@ -7,11 +8,43 @@ int main(int argc, char *argv[]) {
 
 }
 
-void RungeKutta(double alph, double bett, double C[][]) {
-	// here will be formulas, which count value R-K in points
+void RungeKutta(int n, double alph, double bett, double *u[]) { // this func count value of u in spec points
+	double C[4][4];
+	double h = 1. / n;
+	
+	u[0][0] = 0;
+	u[1][0] = 0;
+	u[2][0] = alph;
+	u[3][0] = bett;
+
+	for (int i = 0; i < n - 1; i ++) {
+		C[0][0] = u[i][1];
+		C[0][1] = u[i][2];
+		C[0][2] = u[i][3];
+		C[0][3] = /*g(x)*/ - u[i][3] - u[i - 1][0] * sin(i*h);
+
+		C[1][0] = u[i][1] + C[0][1] * h / 2.;
+		C[1][1] = u[i][2] + C[0][2] * h / 2.;
+		C[1][2] = u[i][3] + C[0][3] * h / 2.;
+		C[1][3] = /*g(x)*/ - (u[i][3] + C[0][3] * h / 2.) - (u[i][0] + C[0][0] * h / 2.) * sin((i + 1./2.)*h);
+
+		C[2][0] = u[i][1] + C[1][1] * h / 2.;
+		C[2][1] = u[i][2] + C[1][2] * h / 2.;
+		C[2][2] = u[i][3] + C[1][3] * h / 2.;
+		C[2][3] = /*g(x)*/ - (u[i][3] + C[1][3] * h / 2.) - (u[i][0] + C[1][0] * h / 2.) * sin((i + 1./2.)*h);
+
+		C[3][0] = u[i][1] + C[0][1] * h / 2.;
+		C[3][1] = u[i][2] + C[0][2] * h / 2.;
+		C[3][2] = u[i][3] + C[0][3] * h / 2.;
+		C[3][3] = /*g(x)*/ - (u[i][3] + C[0][3] * h) - (u[i][0] + C[0][0] * h) * sin((i + 1)*h);
+
+		for (int j = 0; j < 4; j ++) {
+			u[i + 1][j] = u[i][j] + (h / 2.)*(C[0][j] + 2*C[1][j] + 2*C[2][j] + C[3][j]);
+		}
+	}
 }
 
-double RKRes(int n, int k, double x, double *C[]) { // find value of k-th func in point x
+double RKRes(int n, int k, double x, double *u[]) { // find value of k-th func in point x
     int rb = 1;
 	double h  = 1. / n;
     for (int i = 1; i < n; i++) {
@@ -21,29 +54,30 @@ double RKRes(int n, int k, double x, double *C[]) { // find value of k-th func i
         }
     }
     
-	return (x - h*(rb - 1)) / h * (C[k][rb] - C[k][rb - 1]);
+	return (x - h*(rb - 1)) / h * (u[k][rb] - u[k][rb - 1]);
 }
 
-void findEdge(double *edge) { // func ready, it count edge parametrs
+void findEdge(int n, double *edge) { // func ready, it count edge parametrs
 	
 	int err = 0;
 	double A_0[3][3], b_0[3], A_1[3][3], b_1[3];
 	double abc_0[3], abc_1[3];
 	double A[2][2], b[2];
+	double u[4][n];
 
 	for (int i = 0; i < 3; i ++) {
 		
 		A_0[i][0] = i / 2.;
 		A_0[i][1] = 1 - i / 2.;
 		A_0[i][2] = 1;
-        RungeKutta(A_0[i][0], A_0[i][1]);
-		b_0[i] = RKRes(/*arguments*/);
+        RungeKutta(n, A_0[i][0], A_0[i][1], u);
+		b_0[i] = u[0][n - 1];
 
 		A_1[i][0] = i / 2.;
 		A_1[i][1] = 1 - i / 2.;
 		A_1[i][2] = 1;
-        RungeKutta(A_0[i][0], A_0[i][1]);
-		b_1[i] = RKRes(/*arguments*/);
+        RungeKutta(n, A_1[i][0], A_1[i][1], u);
+		b_1[i] = u[1][n - 1];
 	}
 
 	solveLineralSystem(3, A_0, b_0, abc_0, &err);
